9d0d4f1330a3fa35793da241119232ab6be5a46f
Michael Bridgen <mikeb@squaremobius.net>
2019-11-14 18:02:10 +0000

    Implement GitHub push hook receiver
    This implements the receiver for GitHub push events. Some changes /en
    passant/:
    
     - just use http.Error, rather than the overly fancy flux version of
       the same
     - log to stderr when something fails
     - now loads the key for an endpoint, exercised by the GitHub handler
       which cheks signatures

--- sources.go
+++ sources.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/google/go-github/v28/github"
 
 	fluxapi "github.com/fluxcd/flux/pkg/api"
 	fluxapi_v9 "github.com/fluxcd/flux/pkg/api/v9"
@@ -12,10 +20,13 @@ import (
 	"github.com/fluxcd/flux/pkg/image"
 )
 
+const timeout = 10 * time.Second
+
 type HookHandler func(s fluxapi.Server, key []byte, w http.ResponseWriter, r *http.Request)
 
 var Sources = map[string]HookHandler{
 	"DockerHub": handleDockerhub,
+	"GitHub":    handleGithubPush,
 }
 
 func HandlerFromEndpoint(baseDir, apiUrl string, ep Endpoint) (string, http.Handler, error) {
@@ -28,7 +39,11 @@ func HandlerFromEndpoint(baseDir, apiUrl string, ep Endpoint) (string, http.Hand
 	// 2. load the key so it can be used in the handler, and get the
 	// fingerprint so it can be used to route to this handler
 	// TODO...
-	key := []byte{}
+	key, err := ioutil.ReadFile(filepath.Join(baseDir, ep.KeyPath))
+	if err != nil {
+		return "", nil, fmt.Errorf("cannot load key from %q: %s", ep.KeyPath, err.Error())
+	}
+
 	fingerprint := "abc123"
 
 	apiClient := fluxclient.New(http.DefaultClient, fluxhttp.NewAPIRouter(), apiUrl, fluxclient.Token(""))
@@ -37,6 +52,10 @@ func HandlerFromEndpoint(baseDir, apiUrl string, ep Endpoint) (string, http.Hand
 	return fingerprint, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		sourceHandler(apiClient, key, w, r)
 	}), nil
+}
+
+func log(msg ...interface{}) {
+	fmt.Fprintln(os.Stderr, msg...)
 }
 
 // --- specific handlers
@@ -49,7 +68,8 @@ func handleDockerhub(s fluxapi.Server, _ []byte, w http.ResponseWriter, r *http.
 	}
 	var p payload
 	if err := json.NewDecoder(r.Body).Decode(&p); err != nil {
-		fluxhttp.WriteError(w, r, http.StatusBadRequest, err)
+		http.Error(w, "Cannot decode webhook payload", http.StatusBadRequest)
+		log("DockerHub", err.Error())
 		return
 	}
 	handleImageNotify(s, w, r, p.Repository.RepoName)
@@ -58,7 +78,8 @@ func handleDockerhub(s fluxapi.Server, _ []byte, w http.ResponseWriter, r *http.
 func handleImageNotify(s fluxapi.Server, w http.ResponseWriter, r *http.Request, img string) {
 	ref, err := image.ParseRef(img)
 	if err != nil {
-		fluxhttp.WriteError(w, r, http.StatusUnprocessableEntity, err)
+		http.Error(w, "Cannot parse image in webhook payload", http.StatusBadRequest)
+		log("could not parse image from hook payload:", img, ":", err.Error())
 		return
 	}
 	change := fluxapi_v9.Change{
@@ -68,9 +89,55 @@ func handleImageNotify(s fluxapi.Server, w http.ResponseWriter, r *http.Request,
 		},
 	}
 	ctx := r.Context()
-	// Ignore the error returned here as the sender doesn't care. We'll log any
-	// errors at the daemon level.
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
 	s.NotifyChange(ctx, change)
+	w.WriteHeader(http.StatusOK)
+}
 
+func handleGithubPush(s fluxapi.Server, key []byte, w http.ResponseWriter, r *http.Request) {
+	payload, err := github.ValidatePayload(r, key)
+	if err != nil {
+		http.Error(w, "The GitHub signature header is invalid.", 401)
+		log("GitHub", "invalid signature:", err.Error())
+		return
+	}
+
+	hook, err := github.ParseWebHook(github.WebHookType(r), payload)
+	if err != nil {
+		http.Error(w, "Cannot parse payload", http.StatusBadRequest)
+		log("GitHub", "could not parse payload:", err.Error())
+		return
+	}
+
+	switch hook := hook.(type) {
+	case *github.PushEvent:
+		update := fluxapi_v9.GitUpdate{
+			URL:    *hook.Repo.SSHURL,
+			Branch: strings.TrimPrefix(*hook.Ref, "refs/heads/"),
+		}
+		change := fluxapi_v9.Change{
+			Kind:   fluxapi_v9.GitChange,
+			Source: update,
+		}
+		ctx := r.Context()
+		ctx, cancel := context.WithTimeout(ctx, timeout)
+		defer cancel()
+
+		err := s.NotifyChange(ctx, change)
+		if err != nil {
+			select {
+			case <-ctx.Done():
+				http.Error(w, "Timed out waiting for response from downstream API", http.StatusRequestTimeout)
+				log("GitHub", "timed out")
+			default:
+				http.Error(w, "Error while calling downstream API", http.StatusInternalServerError)
+				log("GitHub", "error:", err.Error())
+			}
+			return
+		}
+	default:
+		log("GitHub", "unexpected webhook payload", fmt.Sprintf("received webhook: %T\n%s", hook, github.Stringify(hook)))
+	}
 	w.WriteHeader(http.StatusOK)
 }
