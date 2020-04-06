8c39bd18fc9624be13bfee080b1ba6c94d64a259
Jeremy Davis <JDavis4102@gmail.com>
2020-04-06 10:37:47 -0600

    Add Quay Registry Push notification

---
+++ quay.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	fluxapi "github.com/fluxcd/flux/pkg/api"
+)
+
+const Quay = "Quay"
+
+func init() {
+	Sources[Quay] = handleQuay
+}
+
+func handleQuay(s fluxapi.Server, _ []byte, w http.ResponseWriter, r *http.Request) {
+	type payload struct {
+		RepoName string `json:"docker_url"`
+	}
+	var p payload
+	if err := json.NewDecoder(r.Body).Decode(&p); err != nil {
+		http.Error(w, "Cannot decode webhook payload", http.StatusBadRequest)
+		log(Quay, err.Error())
+		return
+	}
+	doImageNotify(s, w, r, p.RepoName)
+}
